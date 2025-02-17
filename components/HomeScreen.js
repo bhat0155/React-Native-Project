165b9f42e6b269156819834c1e0b1763db36aa27
Ekam Bhatia <ekambhatia@Mac.ottawa.ad.algonquincollege.com>
2025-02-17 16:12:20 -0500

    added FAB

--- components/HomeScreen.js
+++ components/HomeScreen.js
@@ -1,21 +1,32 @@
 import { View, Text, Button } from "react-native";
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { URL, ACCESS_TOKEN } from "../constants/constants";
 import { useSearchContext } from "../context/SearchContext";
+import { FAB } from "@rneui/themed";
 
 const HomeScreen = () => {
-  // const { fetchMovies, searchState } = useSearchContext();
+  const [visible, setVisible] = useState(true);
 
-  // useEffect(() => {
-  //   console.log("initial");
-
-  //   fetchMovies(URL, "munna");
-  // }, []);
-  // console.log(searchState);
+  const openDialog = () => {
+    console.log("Fab clicked");
+  };
 
   return (
-    <View>
+    <View style={{ flex: 1 }}>
       <Text>HomeScreen</Text>
+
+      <FAB
+        visible={visible}
+        onPress={openDialog}
+        placement="right"
+        icon={{ name: "search", color: "black" }}
+        color="orange"
+        style={{
+          position: "absolute",
+          bottom: 40,
+          right: 20,
+        }}
+      />
     </View>
   );
 };
