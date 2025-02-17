f4153b812b77af1e35742067d86e3e39ada2dde4
Ekam Bhatia <ekambhatia@Mac.ottawa.ad.algonquincollege.com>
2025-02-17 12:44:17 -0500

    made 3 screen components

---
+++ components/RentScreen.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const RentScreen = () => {
+  return (
+    <View>
+      <Text>RentScreen</Text>
+    </View>
+  )
+}
+
+export default RentScreen
